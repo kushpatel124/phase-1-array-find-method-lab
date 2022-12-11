03c5e3f815f051c5f8f3c900a579636d72ea97ec
Kush <kushpatel124@gmail.com>
2022-12-11 14:50:29 -0500

    commit tree e30370074a4adc1e9ec6c97a80aea003ccdcd439

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  { year: "2012", result: "L" },
+  { year: "2011", result: "L" },
+  { year: "2010", result: "L" },
+]
+
+
+function superbowlWin(array) {
+  for (let item of array) {
+    if (item.result === "W") {
+      return item.year;
+    }
+  }
+}
